b7c31e7316ce0ae211e16e4aeaf517cfa9c2ba84
leejeongseung1 <jki1256@hanmail.net>
2024-03-31 21:47:51 +0900

    리덕스 예제 변경

--- src/App.js
+++ src/App.js
@@ -1,34 +1,117 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {Box} from "./component/Box";
-import React from "react";
+import React, {useState} from "react";
+import {Button, Col, Container, InputGroup, Row, Form} from "react-bootstrap";
 
 function App() {
-    const count = useSelector(state=> state.count);
+    const result = useSelector(state=> state.result);
+    const auth = useSelector(state => state.auth);
+
+    const [idInput, setIdInput] = useState(null);
+    const [pwdInput, setPwdInput] = useState(null);
+    const [adder, setAdder] = useState(0);
+    const [subber, setSubber] = useState(0);
     const dispatch = useDispatch();
 
+    const onAdderInputChange = (e) =>{
+        setAdder(e.target.value);
+    }
+    const onSubberInputChange = (e)=>{
+        setSubber(e.target.value);
+    }
+
+    const onIdInputChange = (e) =>{
+        setIdInput(e.target.value);
+    }
+    const onPwdInputChange = (e)=>{
+        setPwdInput(e.target.value);
+    }
+    
+    
     const countUp = ()=> {
         // type : action's name,
         // payload : action's parameter (is optional)
-        dispatch({type: "INCREMENT", payload:{num:5}});
+        dispatch({type: "INCREMENT", payload:{num:Number(adder)}});
     }
     const countDown = () => {
-        dispatch({type: "DECREMENT"});
+        dispatch({type: "DECREMENT",payload:{num:Number(subber)}});
     }
 
-    const login = ()=>{
-        dispatch({type:"LOGIN", payload:{id:'jki1256',pwd:'123456'}});
+    const login = (e)=>{
+        e.preventDefault();
+        if(!auth){
+            dispatch({type:"LOGIN", payload:{id:idInput,pwd:pwdInput}});
+            setIdInput("");
+            setPwdInput("");
+        }else{
+            alert("이미 로그인 했습니다.");
+        }
+
+
+    }
+    const logout = ()=>{
+        if(auth){
+            dispatch({type:"LOGIN", payload:{id:null,pwd:null}});
+            setIdInput("");
+            setPwdInput("");
+        }else{
+            alert("로그인 상태가 아닙니다.");
+        }
+
     }
 
     return (
-        <div className="App">
-            <div>{count}</div>
-            <br/>
-            <button onClick={countUp}> + count up</button>
-            <button onClick={countDown}> - count down</button>
-            <button onClick={login}>LOGIN</button>
-            <Box/>
-        </div>
+        <Container className="Calculator" fluid={"md"}>
+            <hr/>
+            <h1 className="Result">{result}</h1>
+            <hr/>
+            <Row className="pad">
+                <Col className="OperButton">
+                    <Form>
+                        <InputGroup  style={{margin:"5px"}}>
+                                    <Form.Control type="number" placeholder="더할 값" onChange={onAdderInputChange}></Form.Control>
+                        </InputGroup>
+                    </Form>
+                </Col>
+                <Col className="OperButton">
+                    <Form>
+                        <InputGroup  style={{margin:"5px"}}>
+                                    <Form.Control type="number" placeholder="뺄 값" onChange={onSubberInputChange}></Form.Control>
+                        </InputGroup>
+                    </Form>
+                </Col>
+            </Row>
+            <Row className="pad">
+                <Col className="OperButton" ><Button variant="success"  onClick={countUp}> + count up</Button></Col>
+                <Col className="OperButton" ><Button variant="danger" onClick={countDown}> - count down</Button></Col>
+            </Row>
+            <Row className="pad">
+                <Col className="Info"><Box /></Col>
+                <Col className="OperButton">
+                    <Row>
+                        <Form onSubmit={login}>
+                            <InputGroup style={{margin:"5px"}}>
+                                <Form.Control placeholder="ID" onChange={onIdInputChange} value={idInput}></Form.Control>
+                            </InputGroup>
+                            <InputGroup style={{margin:"5px"}}>
+                                <Form.Control type="password" placeholder="Password" onChange={onPwdInputChange} value={pwdInput}></Form.Control>
+                            </InputGroup>
+                            <Container className="ButtonGroup">
+                                <Button variant="primary" type="submit">LOGIN</Button>
+                                <Button variant="warning" onClick={logout}>LOGOUT</Button>
+                            </Container>
+
+                        </Form>
+
+                    </Row>
+
+                </Col>
+
+            </Row>
+
+
+        </Container>
     );
 }
 
--- src/redux/reducer/reducer.jsx
+++ src/redux/reducer/reducer.jsx
@@ -1,5 +1,7 @@
 let initialState  = {
-    count:0,
+    result:0,
+    idInput:null,
+    pwdInput:null,
     auth:false,
     id:null,
     password:null
@@ -10,10 +12,11 @@ let initialState  = {
 export const Reducer = (state = initialState, action) => {
     switch (action.type){
         case "INCREMENT":
-            if (action.payload) return state = {...state, count:state.count+action.payload.num};
-            return {...state, count:state.count+1};
+            if (action.payload) return state = {...state, result:state.result+action.payload.num};
+            return {...state, result:state.result+1};
         case "DECREMENT":
-            return {...state, count:state.count-1};
+            if (action.payload) return state = {...state, result:state.result-action.payload.num};
+            return {...state, result:state.result-1};
         case "LOGIN":
             console.log(action.payload.id);
             return {...state, id: state.auth ? null : action.payload.id, pwd : state.auth ? null : action.payload.pwd, auth : !state.auth,};
